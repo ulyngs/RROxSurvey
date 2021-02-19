b59f8c9ae431447afe6c4c83559b9d0aa84a9eb1
Malika Ihle <malika_ihle@hotmail.fr>
2021-02-19 17:51:43 +0000

    inventory

--- Analyses.R
+++ Analyses.R
@@ -1619,4 +1619,34 @@ pgrdata_OtherTraining <- pgrdata_OtherTraining[!is.na(pgrdata_OtherTraining$Trai
 }
 
 
+###############
+## Inventory ##
+###############
 
+# pgrdata_Inventory
+{
+  pgrdata_Inventory <- pgrdata[pgrdata$StudentStaff == "Student",  
+                                   grep(pattern="^Inventory", x=colnames(pgrdata))]
+  
+  head(pgrdata_Inventory)
+  pgrdata_Inventory <- data.frame(stack(pgrdata_Inventory))$values
+  pgrdata_Inventory <- pgrdata_Inventory[!is.na(pgrdata_Inventory)]
+  
+  ## Nb of responses
+  length(pgrdata_Inventory)
+  
+}
+
+# pgrdata_staff_Inventory
+{
+  pgrdata_staff_Inventory <- pgrdata[pgrdata$StudentStaff == "Staff",  
+                               grep(pattern="^Inventory", x=colnames(pgrdata))]
+  
+  head(pgrdata_staff_Inventory)
+  pgrdata_staff_Inventory <- data.frame(stack(pgrdata_staff_Inventory))$values
+  pgrdata_staff_Inventory <- pgrdata_staff_Inventory[!is.na(pgrdata_staff_Inventory)]
+  
+  ## Nb of responses
+  length(pgrdata_staff_Inventory)
+  
+}
