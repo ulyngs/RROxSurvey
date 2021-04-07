0e74270eaab1d7a0798214ea4bd92d1786dad782
Malika Ihle <malika_ihle@hotmail.fr>
2021-04-07 10:26:00 +0100

    call data it's specific name (pgrdata_Effect)

--- Effect.R
+++ Effect.R
@@ -10,7 +10,8 @@ Measures
 Effect_columns <- c(expr(Effect_OA), expr(Effect_Data), expr(Effect_Code), expr(Effect_Materials),expr(Effect_Preprint),expr(Effect_Prereg),expr(Effect_RegRep))
 Effect_answers <- c("Beneficial", "Neutral(neither detrimental nor beneficial)", "Detrimental","Not sure", "Not applicable")
 answers_colors <- c("black", "#666666", "#D95F02", "#E6AB02", "#1B9E77")
-title_plot <- 'Effect'
+title_plot <- 'Overall 
+effect'
 
 
 # create datadet for plotting per Divisions -----
@@ -26,17 +27,17 @@ skeleton <- create_skeleton(Measures, Divisions, Effect_answers, Effect_columns)
 summaryitems <- bind_summaries_items(Measures, pgrdata_Effect, Effect_columns)
 
 ## merge summary items to skeleton
-data <- merge(skeleton, summaryitems, by = "ID", all.x = TRUE)
+pgrdata_Effect <- merge(skeleton, summaryitems, by = "ID", all.x = TRUE)
 rm(skeleton, summaryitems, Effect_columns)
 
 
 # plot per Division -----
-pgrdata_Effect_plot <- circular_plot_function(data, Measures, Effect_answers, title_plot, answers_colors)
+pgrdata_Effect_plot <- circular_plot_function(pgrdata_Effect, Measures, Effect_answers, title_plot, answers_colors)
 
 ## ggsave(pgrdata_Effect_plot, file=here("Figures/pgrdata_Effect2.png"), width=10, height=8)
 
 # regroup data split per Division for overall plot -----
-All_pgrdata_Effect <- regroup_all_data(data)
+All_pgrdata_Effect <- regroup_all_data(pgrdata_Effect)
 
 # plot regrouped data  -----
 All_pgrdata_Effect_plot <- stacked_barplot_on_regrouped_data(All_pgrdata_Effect, Effect_answers, answers_colors)
