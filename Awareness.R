3583229bd02f83234c3160faeed2576fa291ce6e
Malika Ihle <malika_ihle@hotmail.fr>
2021-04-07 10:57:14 +0100

    rewrite awareness with functin

--- Awareness.R
+++ Awareness.R
@@ -6,234 +6,39 @@
 rm(list = ls())
 source("FormatPGRdata.R")
 
-###############
-## Awareness ##
-###############
+Measures
+Awareness_columns <- c(expr(Awareness_OA), expr(Awareness_Data), expr(Awareness_Code), expr(Awareness_Materials),expr(Awareness_Preprint),expr(Awareness_Prereg),expr(Awareness_RegRep))
+Awareness_answers <- c("Practicing myself", "Accessing / using only", "Aware only",  "Not aware / not sure if applicable",  "Not applicable" )
+answers_colors <- rev(c("#ABDDA4", "#FFFFBF", "#FDAE61", "#D7191C", "black"))
+title_plot <- 'Awareness'
 
-# pgrdata_Awareness
-{
+
+# create datadet for plotting per Divisions -----
+## select subdataset 
 pgrdata_Awareness <- pgrdata[pgrdata$StudentStaff == "Student",  
-                               c(grep("Div", colnames(pgrdata)), grep(pattern="^Awareness", x=colnames(pgrdata)))]
+                          c(grep("Div", colnames(pgrdata)), grep(pattern="^Awareness", x=colnames(pgrdata)))]
 head(pgrdata_Awareness)
 
+## create skeleton of all possible answers
+skeleton <- create_skeleton(Measures, Divisions, Awareness_answers, Awareness_columns)
 
-pgrdata_Awareness_OA <- pgrdata_Awareness[!is.na(pgrdata_Awareness$Awareness_OA),] %>% 
-  group_by(Div, Awareness_OA) %>%
-  summarise (n = n()) %>% 
-  mutate(perc = n / sum(n) * 100 )
-pgrdata_Awareness_OA$ID <- paste(paste(pgrdata_Awareness_OA$Div, "Open Access", sep="_"), pgrdata_Awareness_OA$Awareness_OA, sep ="_")
-head(pgrdata_Awareness_OA)
+## summarise items                                         
+summaryitems <- bind_summaries_items(Measures, pgrdata_Awareness, Awareness_columns)
 
-pgrdata_Awareness_Data <- pgrdata_Awareness[!is.na(pgrdata_Awareness$Awareness_Data),] %>% 
-  group_by(Div, Awareness_Data) %>%
-  summarise (n = n()) %>% 
-  mutate(perc = n / sum(n) * 100 )
-pgrdata_Awareness_Data$ID <- paste(paste(pgrdata_Awareness_Data$Div, "Open Data", sep="_"), pgrdata_Awareness_Data$Awareness_Data, sep ="_")
-head(pgrdata_Awareness_Data)
-
-pgrdata_Awareness_Code <- pgrdata_Awareness[!is.na(pgrdata_Awareness$Awareness_Code),] %>% 
-  group_by(Div, Awareness_Code) %>%
-  summarise (n = n()) %>% 
-  mutate(perc = n / sum(n) * 100 )
-pgrdata_Awareness_Code$ID <- paste(paste(pgrdata_Awareness_Code$Div, "Open Code", sep="_"), pgrdata_Awareness_Code$Awareness_Code, sep ="_")
-head(pgrdata_Awareness_Code)
-
-pgrdata_Awareness_Materials <- pgrdata_Awareness[!is.na(pgrdata_Awareness$Awareness_Materials),] %>% 
-  group_by(Div, Awareness_Materials) %>%
-  summarise (n = n()) %>% 
-  mutate(perc = n / sum(n) * 100 )
-pgrdata_Awareness_Materials$ID <- paste(paste(pgrdata_Awareness_Materials$Div, "Open Materials", sep="_"), pgrdata_Awareness_Materials$Awareness_Materials, sep ="_")
-head(pgrdata_Awareness_Materials)
-
-pgrdata_Awareness_Preprint <- pgrdata_Awareness[!is.na(pgrdata_Awareness$Awareness_Preprint),] %>% 
-  group_by(Div, Awareness_Preprint) %>%
-  summarise (n = n()) %>% 
-  mutate(perc = n / sum(n) * 100 )
-pgrdata_Awareness_Preprint$ID <- paste(paste(pgrdata_Awareness_Preprint$Div, "Preprint", sep="_"), pgrdata_Awareness_Preprint$Awareness_Preprint, sep ="_")
-head(pgrdata_Awareness_Preprint)
-
-pgrdata_Awareness_Prereg <- pgrdata_Awareness[!is.na(pgrdata_Awareness$Awareness_Prereg),] %>% 
-                                  group_by(Div, Awareness_Prereg) %>%
-                                  summarise (n = n()) %>% 
-                                  mutate(perc = n / sum(n) * 100 )
-pgrdata_Awareness_Prereg$ID <- paste(paste(pgrdata_Awareness_Prereg$Div, "Preregistration", sep="_"), pgrdata_Awareness_Prereg$Awareness_Prereg, sep ="_")
-head(pgrdata_Awareness_Prereg)
-
-pgrdata_Awareness_RegRep <- pgrdata_Awareness[!is.na(pgrdata_Awareness$Awareness_RegRep),] %>% 
-                                  group_by(Div, Awareness_RegRep) %>%
-                                  summarise (n = n()) %>% 
-                                  mutate(perc = n / sum(n) * 100 )
-pgrdata_Awareness_RegRep$ID <- paste(paste(pgrdata_Awareness_RegRep$Div, "Registered Report", sep="_"), pgrdata_Awareness_RegRep$Awareness_RegRep, sep ="_")
-head(pgrdata_Awareness_RegRep)
-
-prgdata_Awareness_Freq <- data.frame(rbind(pgrdata_Awareness_OA[, c('ID', 'n', 'perc')],
-                                             pgrdata_Awareness_Data[, c('ID', 'n', 'perc')], 
-                                             pgrdata_Awareness_Code[, c('ID', 'n', 'perc')],
-                                             pgrdata_Awareness_Materials[, c('ID', 'n', 'perc')],
-                                             pgrdata_Awareness_Preprint[, c('ID', 'n', 'perc')],
-                                             pgrdata_Awareness_Prereg[, c('ID', 'n', 'perc')],
-                                             pgrdata_Awareness_RegRep[, c('ID', 'n', 'perc')]))
-
-rm(pgrdata_Awareness_OA,pgrdata_Awareness_Data,pgrdata_Awareness_Code,pgrdata_Awareness_Materials,pgrdata_Awareness_Preprint,pgrdata_Awareness_Prereg, pgrdata_Awareness_RegRep)
-
-# create structure with temp variables
-Div <- rep(unique(pgrdata_Awareness$Div), each = 35) # 5 divisions
-LabelIndiv <- rep(Measures, each = 5, times = 5) # 7 measures
-Indiv <-paste(Div, LabelIndiv, sep ="_") 
-Answer <- rep(unique(pgrdata_Awareness$Awareness_OA[!is.na(pgrdata_Awareness$Awareness_OA)]), times= 35) # 5 possible answers - remove 'NA'/ blank answers / skipped items
-ID <- paste(Indiv, Answer, sep="_")
-  
-dataframe <- data.frame(ID, Indiv, Div, LabelIndiv, Answer)
-rm(Div, LabelIndiv, Indiv, Answer, ID)
-
-prgdata_Awareness_Freq <- merge(dataframe, prgdata_Awareness_Freq, by = "ID", all.x = TRUE)
-pgrdata_Awareness <- prgdata_Awareness_Freq[, names(prgdata_Awareness_Freq) != "ID"]
-rm(dataframe, prgdata_Awareness_Freq)
-}
-
-head(pgrdata_Awareness)
-nrow(pgrdata_Awareness) # need to be 175
-str(pgrdata_Awareness)
-
-# plot per Div pgrdata_Awareness
-{
-## https://www.r-graph-gallery.com/299-circular-stacked-barplot.html
-data <- pgrdata_Awareness
-data$LabelIndiv <- factor(data$LabelIndiv, levels = Measures) # this will determine order of the bars
-  
-str(data)
-
-# Set a number of 'empty bar' to add at the end of each Div
-empty_bar <- 2
-nObsType <- nlevels(data$Answer)
-to_add <- data.frame( matrix(NA, empty_bar*nlevels(data$Div)*nObsType, ncol(data)) )
-colnames(to_add) <- colnames(data)
-to_add$Div <- rep(levels(data$Div), each=empty_bar*nObsType )
-data <- rbind(data, to_add)
-data <- data %>% arrange(Div, LabelIndiv) # this will determine order of the bars
-data$id <- rep( seq(1, nrow(data)/nObsType) , each=nObsType) # this is x in the plot - determines the order of the bars
-rm(to_add)
-
-# Get the name and the y position of each label
-label_data <- data %>% group_by(id, Indiv) %>% summarize(tot=sum(perc, na.rm=TRUE)) # here all at 100% to plot percents and not counts
-number_of_bar <- nrow(label_data)
-angle <- 90 - 360 * (label_data$id-0.5) /number_of_bar     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
-label_data$hjust <- ifelse(angle < -90, 1, 0)
-label_data$angle <- ifelse(angle < -90, angle+180, angle)
-label_data$LabelIndiv <- sapply(strsplit(as.character(label_data$Indiv),"\\_"), `[`, 2)
-
-# prepare a data frame for base lines
-base_data <- data %>% 
-  group_by(Div) %>% 
-  summarize(start=min(id), end=max(id) - empty_bar) %>% 
-  rowwise() %>% 
-  mutate(title=mean(c(start, end)))
-
-# prepare a data frame for grid (scales)
-grid_data <- base_data
-grid_data$end <- grid_data$end[ c( nrow(grid_data), 1:nrow(grid_data)-1)] + 1
-grid_data$start <- grid_data$start - 1
-grid_data <- grid_data[-1,]
+## merge summary items to skeleton
+pgrdata_Awareness <- merge(skeleton, summaryitems, by = "ID", all.x = TRUE)
+rm(skeleton, summaryitems, Awareness_columns)
 
 
-
-
-
-## Make the plot
-pgrdata_Awareness_plot <- ggplot(data) +      
-  
-  ### Add the stacked bar
-  geom_bar(aes(x=as.factor(id), y=perc, fill=factor(Answer, level = c("Practicing myself", "Accessing / using only", "Aware only",  "Not aware / not sure if applicable",  "Not applicable" ))), stat="identity", alpha=0.5) +
-
-  ### Add a val=100/75/50/25 lines. I do it at the beginning to make sur barplots are OVER it.
-  geom_segment(data=grid_data, aes(x = end, y = 0, xend = start, yend = 0), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) + 
-  geom_segment(data=grid_data, aes(x = end, y = 25, xend = start, yend = 25), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 50, xend = start, yend = 50), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 75, xend = start, yend = 75), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = end, y = 100, xend = start, yend = 100), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = number_of_bar-0.3, y = 100, xend = number_of_bar, yend = 100), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = number_of_bar-1, y = 100, xend = number_of_bar, yend = 100), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = number_of_bar-1, y = 75, xend = number_of_bar, yend = 75), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = number_of_bar-1, y = 50, xend = number_of_bar, yend = 50), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = number_of_bar-1, y = 25, xend = number_of_bar, yend = 25), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  geom_segment(data=grid_data, aes(x = number_of_bar-1, y = 0, xend = number_of_bar, yend = 0), colour = "grey", alpha=1, size=0.3 , inherit.aes = FALSE ) +
-  
-  ### Add text showing the value of each lines max(data$id-0.1)
-  ggplot2::annotate("text", x = rep(number_of_bar-0.5,5), y = c(0, 25, 50, 75, 100), label = c("0%", "25%", "50%", "75%", "100%") , color="grey", size=3 , angle=0, fontface="bold", hjust=c(0.5,0.5,0.5,0.5,0.5), vjust = -0.2) +
-  
-  #scale_fill_manual(values = c("black", "#B2182B", "#F4A582", "#F7F7F7", "#053061"), # https://www.datanovia.com/en/blog/top-r-color-palettes-to-know-for-great-data-visualization/
-    #                breaks=c("Not applicable", "Not aware / not sure if applicable", "Aware only", "Accessing / using only", "Practicing myself"))+
-  
-  #scale_fill_manual(values = c("black", "#D73027", "#FDAE61", "#FFFFBF", "#313695"), # https://www.datanovia.com/en/blog/top-r-color-palettes-to-know-for-great-data-visualization/
-     #               breaks=c("Not applicable", "Not aware / not sure if applicable", "Aware only", "Accessing / using only", "Practicing myself"))+
- 
-   scale_fill_manual(values = c("black", "#D7191C", "#FDAE61", "#FFFFBF", "#ABDDA4"), # https://www.datanovia.com/en/blog/top-r-color-palettes-to-know-for-great-data-visualization/
-                    breaks=c("Not applicable", "Not aware / not sure if applicable", "Aware only", "Accessing / using only", "Practicing myself"))+
-  
-  #scale_fill_manual(values = c("#2B83BA", "#ABDDA4", "#FFFFBF", "#FDAE61", "#D7191C"), # https://www.datanovia.com/en/blog/top-r-color-palettes-to-know-for-great-data-visualization/
-   #                 breaks=c("Not applicable", "Not aware / not sure if applicable", "Aware only", "Accessing / using only", "Practicing myself"))+
-  
-  scale_x_discrete(expand = c(0, 0)) + # remove padding
-  ylim(-100,150) +
-  theme_minimal() +
-  theme(
-    legend.position = "bottom",
-    legend.title=element_blank(),
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank()
-   # plot.margin = unit(rep(1,4), "cm") 
-  ) +
-  coord_polar() +
-  
-  ### Add labels on top of each bar
- geom_text(data=label_data, aes(x=id, y=105, label=LabelIndiv, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=3, angle= label_data$angle, inherit.aes = FALSE ) +
-  
-  ### Add base line information
- geom_segment(data=base_data, aes(x = start, y = -5, xend = end, yend = -5), colour = "black", alpha=0.8, size=0.6 , inherit.aes = FALSE )  +
- geom_text(data=base_data, aes(x = title, y = -20, label=Div), hjust=c(1,1,0.5,0, 0), colour = "black", alpha=0.8, size=4, fontface="bold", inherit.aes = FALSE) +
-
-  ### Add title in the middle
-  ggplot2::annotate("text", x = 0, y = -90, label = "Awareness" , color="black", size=5 , angle=0, fontface="bold", hjust=0.5) 
-    
+# plot per Division -----
+pgrdata_Awareness_plot <- circular_plot_function(pgrdata_Awareness, Measures, Awareness_answers, title_plot, answers_colors)
 pgrdata_Awareness_plot
+## ggsave(pgrdata_Awareness_plot, file=here("Figures/pgrdata_Awareness2.png"), width=10, height=8)
 
-## Save at png
-# ggsave(pgrdata_Awareness_plot, file=here("Figures/pgrdata_Awareness.png"), width=10, height=8)
+# regroup data split per Division for overall plot -----
+All_pgrdata_Awareness <- regroup_all_data(pgrdata_Awareness)
 
-## clean up
-#rm(data, base_data, grid_data, label_data, number_of_bar, nObsType, empty_bar, angle)
-
-}
-
-
-# plot for all pgrdata_Awareness
-{
-All_pgrdata_Awareness <- pgrdata_Awareness[,c("LabelIndiv", "Answer", "n")] %>% group_by(LabelIndiv, Answer) %>% summarise (n = sum(n, na.rm=TRUE)) 
-All_pgrdata_Awareness <- All_pgrdata_Awareness  %>% group_by(LabelIndiv) %>% mutate(perc = n / sum(n) * 100 )
-
-All_pgrdata_Awareness$LabelIndiv <- factor(All_pgrdata_Awareness$LabelIndiv, levels = Measures) # this will determine order of the bars
-str(All_pgrdata_Awareness)
-
-All_pgrdata_Awareness_plot <- ggplot(All_pgrdata_Awareness) +      
-  
-  ### Add the stacked bar
-  geom_bar(aes(x=LabelIndiv, y=perc, fill=factor(Answer, 
-                                                    level = c("Practicing myself", "Accessing / using only", "Aware only",  "Not aware / not sure if applicable",  "Not applicable" ))), 
-           stat="identity", alpha=0.5) +
- 
-  scale_fill_manual(values = c("#ABDDA4", "#FFFFBF", "#FDAE61", "#D7191C", "black"), 
-                    breaks=c("Practicing myself", "Accessing / using only", "Aware only", "Not aware / not sure if applicable", "Not applicable"))+
-  
-  
-theme_minimal() +
-  theme(
-    legend.position = "right",
-    axis.title = element_blank(),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    legend.title=element_blank())
-
+# plot regrouped data  -----
+All_pgrdata_Awareness_plot <- stacked_barplot_on_regrouped_data(All_pgrdata_Awareness, Measures, Awareness_answers, answers_colors)
 All_pgrdata_Awareness_plot
-
-}
+## ggsave(All_pgrdata_Support_plot, file=here("Figures/All_pgrdata_Supportfunctions.png"), width=10, height=8)
